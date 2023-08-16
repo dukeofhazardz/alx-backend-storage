abb8bc352ac87e17fb351e0ef1946ddb10683691
Daniel John <dj71286@gmail.com>
2023-08-16 19:09:39 +0100

    Completed mandatory tasks

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -9,7 +9,7 @@ from typing import Callable, Union
 
 
 def count_calls(method: Callable) -> Callable:
-    """ A that takes a single method Callable argument
+    """ A function that takes a single method Callable argument
         and returns a Callable """
     @wraps(method)
     def wrapper(self, *args, **kwargs):
@@ -17,6 +17,24 @@ def count_calls(method: Callable) -> Callable:
         key = method.__qualname__
         self._redis.incr(key)
         return method(self, *args, **kwargs)
+    return wrapper
+
+def call_history(method: Callable) -> Callable:
+    """ A function that stores the history of inputs and outputs
+        for a particular function. """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """ The wrapper method """
+        key_inputs = "{}:inputs".format(method.__qualname__)
+        key_outputs = "{}:outputs".format(method.__qualname__)
+        input_str = str(args)
+        redis_instance = self._redis
+        redis_instance.rpush(key_inputs, input_str)
+
+        result = method(self, *args, **kwargs)
+        redis_instance.rpush(key_outputs, result)
+
+        return result
     return wrapper
 
 
@@ -29,6 +47,7 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: any) -> str:
         """ A method that takes a data argument and returns a string. """
         key: str = str(uuid4())
@@ -53,3 +72,18 @@ class Cache:
         """ A method that parametrize Cache.get with the int
             conversion function """
         return self.get(key, fn=int)
+
+def replay(method: Callable) -> None:
+    """ A function to display the history of calls of a particular function """
+    key_inputs = "{}:inputs".format(method.__qualname__)
+    key_outputs = "{}:outputs".format(method.__qualname__)
+
+    redis_instance = method.__self__._redis
+    inputs = redis_instance.lrange(key_inputs, 0, -1)
+    outputs = redis_instance.lrange(key_outputs, 0, -1)
+
+    print(f"{method.__qualname__} was called {len(inputs)} times:")
+    for input_str, output_str in zip(inputs, outputs):
+        input_str = input_str.decode('utf-8')
+        output_str = output_str.decode('utf-8')
+        print(f"{method.__qualname__}(*{input_str}) -> {output_str}")
--- 0x02-redis_basic/main.py
+++ 0x02-redis_basic/main.py
@@ -35,3 +35,24 @@ print(cache.get(cache.store.__qualname__))
 cache.store(b"second")
 cache.store(b"third")
 print(cache.get(cache.store.__qualname__))
+
+# 4
+s1 = cache.store("first")
+print(s1)
+s2 = cache.store("secont")
+print(s2)
+s3 = cache.store("third")
+print(s3)
+
+inputs = cache._redis.lrange("{}:inputs".format(cache.store.__qualname__), 0, -1)
+outputs = cache._redis.lrange("{}:outputs".format(cache.store.__qualname__), 0, -1)
+
+print("inputs: {}".format(inputs))
+print("outputs: {}".format(outputs))
+
+# 5
+replay = __import__('exercise').replay
+print(cache.store("foo"))
+print(cache.store("bar"))
+print(cache.store(42))
+print(replay(cache.store))
